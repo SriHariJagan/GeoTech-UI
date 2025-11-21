9359000440d7dc8cf7df5f7e601d09dc928833cf
Sri Hari Jagan <100404902+SriHariJagan@users.noreply.github.com>
2025-11-21 14:18:34 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
-import Store from "./store/store.jsx";
+import Store from "./store/Store";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
